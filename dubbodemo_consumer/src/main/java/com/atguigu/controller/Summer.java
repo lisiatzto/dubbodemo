d328d238ab0ee2c1d16898b4d3906a472fedf94e
赵晓军 <330792247@qq.com>
2022-07-03 20:52:37 +0800

    新增两条代码

--- dubbodemo_consumer/src/main/java/com/atguigu/controller/Summer.java
+++ dubbodemo_consumer/src/main/java/com/atguigu/controller/Summer.java
@@ -4,5 +4,7 @@ public class Summer {
     public static void main(String[] args) {
         System.out.println(" 6666666666 DEV ");
         System.out.println(" 6666666666 master");
+        System.out.println(" 123456");
+        System.out.println(" 123456");
     }
 }
