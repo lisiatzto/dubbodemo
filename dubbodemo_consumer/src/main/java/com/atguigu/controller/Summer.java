07137462b394b01e744eb841c6ded24b688eccf4
赵晓军 <330792247@qq.com>
2022-07-03 18:29:31 +0800

    修改了第5行代码，增加了master

--- dubbodemo_consumer/src/main/java/com/atguigu/controller/Summer.java
+++ dubbodemo_consumer/src/main/java/com/atguigu/controller/Summer.java
@@ -2,6 +2,6 @@ package com.atguigu.controller;
 
 public class Summer {
     public static void main(String[] args) {
-        System.out.println(" 6666666666 ");
+        System.out.println(" 6666666666 master");
     }
 }
